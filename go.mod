50de27c43b1a1b8a865f6f9ac314647d942bf83c
John Deng <john.deng@qq.com>
2021-02-25 17:00:27 +0800

    init commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module demo
+
+go 1.16
+
+require (
+	k8s.io/apimachinery v0.0.0-20190413052414-40a3f73b0fa2
+	k8s.io/client-go v0.0.0-20190413052642-108c485f896e
+	k8s.io/klog v0.0.0-20190306015804-8e90cee79f82
+)
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"path/filepath"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog"
+)
+
+var (
+	masterURL  string
+	kubeconfig string
+)
+
+func main() {
+	flag.StringVar(&kubeconfig, "kubeconfig", defaultKubeconfig(), "Path to a kubeconfig. Only required if out-of-cluster.")
+	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+
+	klog.InitFlags(nil)
+
+	flag.Parse()
+
+	cfg, err := rest.InClusterConfig()
+	if err != nil {
+		cfg, err = clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
+		if err != nil {
+			klog.Fatalf("Error building kubeconfig: %s", err.Error())
+		}
+	}
+
+	clientset, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+		return
+	}
+
+	nl, e := clientset.CoreV1().Nodes().List(v1.ListOptions{})
+	klog.Infof("%+v, %+v", nl, e)
+
+}
+
+func defaultKubeconfig() string {
+	fname := os.Getenv("KUBECONFIG")
+	if fname != "" {
+		return fname
+	}
+	home, err := os.UserHomeDir()
+	if err != nil {
+		klog.Warningf("failed to get home directory: %v", err)
+		return ""
+	}
+	return filepath.Join(home, ".kube", "config")
+}
